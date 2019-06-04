6fbad75e84424c76f2d37da52a258a3f7bb4ea4e
Espen Meidell <espen.meidell@gmail.com>
2019-06-04 15:41:43 +0200

    Create signup form

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import styles from "./SignUp.module.css";
 
 const SignUp = props => {
   return (
-    <div>
-      <h2>Registrer Deg!</h2>
+    <div className={styles.container}>
+      <form>
+        <h2>Registrer Deg!</h2>
+        <div className={styles.inputField}>
+          <label htmlFor="name">Navn</label>
+          <input type="text" id="name" />
+        </div>
+        <div className={styles.inputField}>
+          <label htmlFor="email">Epost</label>
+          <input type="email" id="email" />
+        </div>
+        <div className={styles.inputField}>
+          <label htmlFor="pass1">Passord</label>
+          <input type="password" id="pass1" />
+        </div>
+        <div className={styles.inputField}>
+          <label htmlFor="pass2">Gjenta passord</label>
+          <input type="password" id="pass2" />
+        </div>
+        <div className={styles.inputField}>
+          <input type="submit" value="Registrer" />
+        </div>
+      </form>
     </div>
   );
 };
